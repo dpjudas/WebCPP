b8903efa502bd60f3c65b113d2b0adc051898cf1
Magnus Norddahl <dpjudas@users.noreply.github.com>
2025-07-03 23:34:05 +0200

    Improve intellisense parsing

--- WebCPP/Include/WebCPP/Core/JSValue.h
+++ WebCPP/Include/WebCPP/Core/JSValue.h
@@ -18,14 +18,19 @@ namespace web
 		static JSValue global(const std::string& name);
 		static JSValue object();
 		static JSValue undefined();
+		template<typename T> static JSValue array(const std::vector<T>& items);
 		template<typename... Args> JSValue new_(Args... args) const;
 		JSValue operator[](const std::string& name) const;
+		JSValue operator()(const std::string& name) const;
 		template<typename T> void set(const std::string& name, T value);
 		template<typename RetVal, typename... Args> RetVal call(const std::string& name, Args... args) const;
 		template<typename RetVal> RetVal as() const;
 		bool isNull() const;
 		bool isString() const;
+		bool isNumber() const;
+		bool isUndefined() const;
 	};
+	template<typename T> std::vector<T> vecFromJSArray(JSValue);
 #endif
 
 	static std::string stringify(const JSValue& value)
