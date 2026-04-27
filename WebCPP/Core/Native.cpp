a19ebc610aa2c11736876813fece0bf6bbf2d382
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-04-27 11:31:36 +0200

    Use JsonValue instead of JSValue for native calls and add task version

--- WebCPP/Core/Native.cpp
+++ WebCPP/Core/Native.cpp
@@ -14,11 +14,50 @@ namespace web
 		return !JSValue::global("window")["cefQuery"].isUndefined();
 	}
 
-	void Native::query(JSValue request, std::function<void(JSValue response)> onSuccess, std::function<void(int code, JSValue message)> onFailure)
+	task<JsonValue> Native::query(const JsonValue& request)
+	{
+		auto promise = std::make_shared<web::task_promise<web::JsonValue>>();
+
+		auto successHandler = std::make_unique<JSCallback>([=](JSValue args) -> JSValue
+			{
+				auto responseJson = web::JsonValue::parse(args[0].as<std::string>());
+				promise->set_value(responseJson);
+				return JSValue::undefined();
+			});
+
+		auto failureHandler = std::make_unique<JSCallback>([=](JSValue args) -> JSValue
+			{
+				try
+				{
+					int code = args[0].as<int>();
+					std::string message = args[1].as<std::string>();
+					throw NativeException(code, message);
+				}
+				catch (...)
+				{
+					promise->set_exception(std::current_exception());
+				}
+				return JSValue::undefined();
+			});
+
+		JSValue args = JSValue::object();
+		args.set("request", request.to_json());
+		args.set("persistent", false);
+		args.set("onSuccess", successHandler->getHandler());
+		args.set("onFailure", failureHandler->getHandler());
+
+		pendingRequests.push_back(std::move(successHandler));
+		pendingRequests.push_back(std::move(failureHandler));
+
+		JSValue::global("window").call<void>("cefQuery", args);
+		return promise->get_future();
+	}
+
+	void Native::query(const JsonValue& request, std::function<void(JsonValue response)> onSuccess, std::function<void(int code, std::string message)> onFailure)
 	{
 		auto successHandler = std::make_unique<JSCallback>([=](JSValue args) -> JSValue
 			{
-				JSValue response = JSValue::global("JSON").call<JSValue>("parse", args[0]);
+				JsonValue response = JsonValue::parse(args[0].as<std::string>());
 				if (onSuccess)
 					onSuccess(response);
 				return JSValue::undefined();
@@ -27,14 +66,14 @@ namespace web
 		auto failureHandler = std::make_unique<JSCallback>([=](JSValue args) -> JSValue
 			{
 				int code = args[0].as<int>();
-				JSValue response = (code != -1) ? JSValue::global("JSON").call<JSValue>("parse", args[1]) : args[1];
+				std::string message = args[1].as<std::string>();
 				if (onFailure)
-					onFailure(code, response);
+					onFailure(code, message);
 				return JSValue::undefined();
 			});
 
 		JSValue args = JSValue::object();
-		args.set("request", JSValue::global("JSON").call<JSValue>("stringify", request));
+		args.set("request", request.to_json());
 		args.set("persistent", false);
 		args.set("onSuccess", successHandler->getHandler());
 		args.set("onFailure", failureHandler->getHandler());
@@ -45,11 +84,11 @@ namespace web
 		JSValue::global("window").call<void>("cefQuery", args);
 	}
 
-	int Native::subscribe(JSValue request, std::function<void(JSValue message)> onMessage, std::function<void(int code, JSValue message)> onUnsubscribe)
+	int Native::subscribe(const JsonValue& request, std::function<void(JsonValue message)> onMessage, std::function<void(int code, std::string message)> onUnsubscribe)
 	{
 		auto successHandler = std::make_unique<JSCallback>([=](JSValue args) -> JSValue
 			{
-				JSValue response = JSValue::global("JSON").call<JSValue>("parse", args[0]);
+				JsonValue response = JsonValue::parse(args[0].as<std::string>());
 				if (onMessage)
 					onMessage(response);
 				return JSValue::undefined();
@@ -58,14 +97,14 @@ namespace web
 		auto failureHandler = std::make_unique<JSCallback>([=](JSValue args) -> JSValue
 			{
 				int code = args[0].as<int>();
-				JSValue response = (code != -1) ? JSValue::global("JSON").call<JSValue>("parse", args[1]) : args[1];
+				std::string message = args[1].as<std::string>();
 				if (onUnsubscribe)
-					onUnsubscribe(code, response);
+					onUnsubscribe(code, message);
 				return JSValue::undefined();
 			});
 
 		JSValue args = JSValue::object();
-		args.set("request", JSValue::global("JSON").call<JSValue>("stringify", request));
+		args.set("request", request.to_json());
 		args.set("persistent", true);
 		args.set("onSuccess", successHandler->getHandler());
 		args.set("onFailure", failureHandler->getHandler());
--- WebCPP/Include/WebCPP/Core/Native.h
+++ WebCPP/Include/WebCPP/Core/Native.h
@@ -1,17 +1,29 @@
 #pragma once
 
+#include <string>
 #include <vector>
 #include <functional>
+#include <stdexcept>
 #include "JSValue.h"
+#include "Task.h"
+#include "JsonValue.h"
 
 namespace web
 {
+	class NativeException : public std::runtime_error
+	{
+	public:
+		NativeException(int code, const std::string& message) : std::runtime_error(message), code(code) {}
+		int code = 0;
+	};
+
 	class Native
 	{
 	public:
 		static bool isApp();
-		static void query(JSValue request, std::function<void(JSValue response)> onSuccess = {}, std::function<void(int code, JSValue message)> onFailure = {});
-		static int subscribe(JSValue request, std::function<void(JSValue message)> onMessage, std::function<void(int code, JSValue message)> onUnsubscribe = {});
+		static void query(const JsonValue& request, std::function<void(JsonValue response)> onSuccess, std::function<void(int code, std::string message)> onFailure = {});
+		static task<JsonValue> query(const JsonValue& request);
+		static int subscribe(const JsonValue& request, std::function<void(JsonValue message)> onMessage, std::function<void(int code, std::string message)> onUnsubscribe = {});
 		static void unsubscribe(int id);
 	};
 }
