6bf5c200af77c215f7e26084afc3c8dd7da06c02
Jacob Pedersen <github@bleze.dk>
2026-05-19 08:00:14 +0200

    Added sendRequestBinary

--- WebCPP/Include/WebCPP/Core/SendRequest.h
+++ WebCPP/Include/WebCPP/Core/SendRequest.h
@@ -4,12 +4,15 @@
 #include "JsonValue.h"
 #include "Task.h"
 #include <functional>
+#include <vector>
 
 namespace web
 {
 	task<JsonValue> sendRequest(std::string url, const JsonValue& request);
+	task<std::vector<uint8_t>> sendRequestBinary(std::string url, const JsonValue& request);
 
 	void sendRequest(std::string url, const JsonValue& request, std::function<void(JsonValue response)> responseHandler, std::function<void(int statusCode, std::string contentType, std::string body)> errorHandler = {});
+	void sendRequestBinary(std::string url, const JsonValue& request, std::function<void(std::vector<uint8_t> response)> responseHandler, std::function<void(int statusCode, std::string contentType, std::string body)> errorHandler = {});
 	void setDefaultRequestErrorHandler(std::function<void(int statusCode, std::string contentType, std::string body)> errorHandler);
 	void callDefaultRequestErrorHandler(int statusCode, std::string contentType, std::string body);
 	void sendRequestSaveResponse(std::string url, const JsonValue& request, const std::string& filename, std::function<void(int statusCode, std::string contentType, std::string body)> errorHandler = {});
